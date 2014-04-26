24e933fc90b7bed2b2ceb0ba50a5444d68e9085a
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2014-04-26 10:34:25 +0200

    Remove menu & clean MainActivity

--- Android/app/src/main/java/fr/tvbarthel/apps/adaptilo/MainActivity.java
+++ Android/app/src/main/java/fr/tvbarthel/apps/adaptilo/MainActivity.java
@@ -1,13 +1,7 @@
 package fr.tvbarthel.apps.adaptilo;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 import fr.tvbarthel.apps.adaptilo.fragments.BasicControllerFragment;
 import fr.tvbarthel.apps.adaptilo.fragments.ControllerFragment;
@@ -27,43 +21,8 @@ public class MainActivity extends FragmentActivity implements ControllerFragment
         }
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     @Override
     public void sendMessage(Message messageToSend) {
         // TODO handle the message
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
     }
 }
