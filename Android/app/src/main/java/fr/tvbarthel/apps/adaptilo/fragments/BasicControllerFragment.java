e9bdd088afc3c7fb48c7b637ac52dbec8cd515e3
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2014-04-27 09:33:08 +0200

    Use a custom TypeFace

--- Android/app/src/main/java/fr/tvbarthel/apps/adaptilo/fragments/BasicControllerFragment.java
+++ Android/app/src/main/java/fr/tvbarthel/apps/adaptilo/fragments/BasicControllerFragment.java
@@ -1,9 +1,11 @@
 package fr.tvbarthel.apps.adaptilo.fragments;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import fr.tvbarthel.apps.adaptilo.R;
 import fr.tvbarthel.apps.adaptilo.models.Message;
@@ -13,6 +15,11 @@ public class BasicControllerFragment extends AdaptiloControllerFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View fragmentView = inflater.inflate(R.layout.fragment_basic_controller, container, false);
+        Typeface minecraftiaTypeFace = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Minecraftia.ttf");
+        ((Button) fragmentView.findViewById(R.id.basic_controller_btn_start)).setTypeface(minecraftiaTypeFace);
+        ((Button) fragmentView.findViewById(R.id.basic_controller_btn_select)).setTypeface(minecraftiaTypeFace);
+        ((Button) fragmentView.findViewById(R.id.basic_controller_btn_a)).setTypeface(minecraftiaTypeFace);
+        ((Button) fragmentView.findViewById(R.id.basic_controller_btn_b)).setTypeface(minecraftiaTypeFace);
         return fragmentView;
     }
 
