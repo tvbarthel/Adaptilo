751bc8530f575fdee2f96a8106ab7a5ef7979ef2
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2014-04-29 21:34:35 +0200

    Finalize qr code scanning process

--- Android/app/src/main/java/fr/tvbarthel/apps/adaptilo/helpers/CaptureHelper.java
+++ Android/app/src/main/java/fr/tvbarthel/apps/adaptilo/helpers/CaptureHelper.java
@@ -3,18 +3,28 @@ package fr.tvbarthel.apps.adaptilo.helpers;
 import android.app.Activity;
 import android.content.Intent;
 
+import com.google.zxing.client.android.CaptureActivity;
 import com.google.zxing.integration.android.IntentIntegrator;
 
-import fr.tvbarthel.apps.adaptilo.activities.BasicControllerCaptureActivity;
-
+/**
+ * An helper class used to start a {@link com.google.zxing.client.android.CaptureActivity}
+ */
 public final class CaptureHelper {
 
-    public static void initiateScan(Activity activity, CharSequence stringDesiredBarcodeFormats, String prompt) {
-        Intent intent = IntentIntegrator.createScanIntent(activity, stringDesiredBarcodeFormats, prompt);
-        intent.setClass(activity, BasicControllerCaptureActivity.class);
-        activity.startActivityForResult(intent, IntentIntegrator.REQUEST_CODE);
+    /**
+     * Start an activity for scanning a QrCode.
+     *
+     * @param startActivity        the {@link android.app.Activity} calling startActivityForResult.
+     * @param scannerActivityClass the activity that will be launched.
+     * @param prompt               the text to be displayed on screen.
+     */
+    public static void initiateQrCodeScan(Activity startActivity, Class<? extends CaptureActivity> scannerActivityClass, String prompt) {
+        Intent intent = IntentIntegrator.createScanIntent(startActivity, IntentIntegrator.QR_CODE_TYPES, prompt);
+        intent.setClass(startActivity, scannerActivityClass);
+        startActivity.startActivityForResult(intent, IntentIntegrator.REQUEST_CODE);
     }
 
+    // Non instantiable class.
     private CaptureHelper() {
     }
 }
