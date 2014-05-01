825c3d08486f28507ea79afb0e00b12d9472e4ca
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2014-05-01 17:22:21 +0200

    Simple Server : first code drop

---
+++ Server/SimpleServer/src/main/java/fr/tvbarthel/apps/adaptilo/server/Launcher.java
@@ -0,0 +1,9 @@
+package fr.tvbarthel.apps.adaptilo.server;
+
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        System.out.println("Simple Server Launcher");
+    }
+}
