37148c6514f0ee8614801cfdc1b2d1adebe0b42a
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2014-05-22 07:38:23 +0200

    Add vibrate and vibrate pattern test from server

--- Server/SimpleServer/src/main/java/fr/tvbarthel/apps/adaptilo/server/AdaptiloServer.java
+++ Server/SimpleServer/src/main/java/fr/tvbarthel/apps/adaptilo/server/AdaptiloServer.java
@@ -3,7 +3,13 @@ package fr.tvbarthel.apps.adaptilo.server;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import fr.tvbarthel.apps.adaptilo.server.helpers.MessageDeserializerHelper;
-import fr.tvbarthel.apps.adaptilo.server.models.*;
+import fr.tvbarthel.apps.adaptilo.server.models.Event;
+import fr.tvbarthel.apps.adaptilo.server.models.Message;
+import fr.tvbarthel.apps.adaptilo.server.models.NetworkMessage;
+import fr.tvbarthel.apps.adaptilo.server.models.RegisterControllerRequest;
+import fr.tvbarthel.apps.adaptilo.server.models.enums.EventAction;
+import fr.tvbarthel.apps.adaptilo.server.models.enums.EventType;
+import fr.tvbarthel.apps.adaptilo.server.models.enums.MessageType;
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
@@ -83,6 +89,24 @@ public class AdaptiloServer extends WebSocketServer {
                 c.send(text);
             }
         }
+    }
+
+    /**
+     * allow to test vibrator pattern
+     * TODO remove, only for test purpose
+     */
+    public void vibratePattern(long[] pattern) {
+        sendToAll(mParser.toJson(new Message(MessageType.VIBRATOR_PATTERN, pattern)));
+    }
+
+    /**
+     * allow to test vibrator
+     * TODO remove, only for test purpose
+     *
+     * @param duration
+     */
+    public void vibrate(Long duration) {
+        sendToAll(mParser.toJson(new Message(MessageType.VIBRATOR, duration)));
     }
 
     /**
--- Server/SimpleServer/src/main/java/fr/tvbarthel/apps/adaptilo/server/Launcher.java
+++ Server/SimpleServer/src/main/java/fr/tvbarthel/apps/adaptilo/server/Launcher.java
@@ -23,6 +23,10 @@ public class Launcher {
                 server.enableShaker(true);
             } else if (in.equals("disable shaker")) {
                 server.enableShaker(false);
+            } else if (in.equals("vibrate")) {
+                server.vibrate(2000l);
+            } else if (in.equals("vibrate pattern")) {
+                server.vibratePattern(new long[]{100, 250, 200, 250, 200, 250, 200, 250, 200, 250});
             } else {
                 System.out.println("sendToAll " + in);
                 server.sendToAll(in);
