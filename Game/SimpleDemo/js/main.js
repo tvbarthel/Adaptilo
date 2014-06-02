d8c93f5622e7cb08f0aaeb44a7be7e05dc7602aa
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2014-06-02 08:32:54 +0200

    Add a JS skeleton

---
+++ Game/SimpleDemo/js/main.js
@@ -0,0 +1,72 @@
+/**
+*    Dependencies
+*       jQuery
+*/
+
+/**
+ *  A simple Adaptilo browser client.
+ *  
+ *  Notice : 
+ *
+ *  import jQuery and this file in your html page.
+ *  <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+ *  <script src="js/main.js"></script>
+ *   
+ *  Create your own configuration or use Adaptilo.defaultConfiguration;
+ *  var myConfiguration = Adaptilo.defaultConfiguration;
+ *
+ *  Instantiate a new platform.
+ *  var myPlatform = Adaptilo.platform(myConfiguration);
+ *
+ *  Connect the platform to the server.
+ *  myPlatform.connect();
+ *
+ */
+
+var Adaptilo = Adaptilo || {};
+
+Adaptilo.defaultConfiguration = (function() {
+    "use strict";
+    return {
+        server_ip           :   "192.168.0.1",
+        server_port         :   "8080",
+        onSocketOpen        :   function(event) {
+            console.log("[Web Socket] onSocketOpen -> " + event);
+        },
+        onSocketClose       :   function(event) {
+            console.log("[Web Socket] onSocketClose -> " + event);
+        },
+        onSocketMessage     :   function(event) {
+            console.log("[Web Socket] onSocketMessage -> " + event);
+        },
+        onSocketError       :   function(event) {
+            console.log("[Web Socket] onSocketError -> " + event);
+        }
+        
+    };
+})();
+
+Adaptilo.platform = (function() {
+    "use strict";
+    
+    //  private attributs
+    var mWebSocket;
+    
+    // constructor
+    var AdaptiloPlatform = function(platformConfiguration) {   
+        this.platformConfiguration = $.extend(true, {}, platformConfiguration);
+    };
+    
+    // Public API
+    AdaptiloPlatform.prototype = {
+        connect :   function() {
+            mWebSocket = new WebSocket('ws://'+this.platformConfiguration.server_ip+':'+this.platformConfiguration.server_port);
+            mWebSocket.onopen    = this.platformConfiguration.onSocketOpen;
+            mWebSocket.onclose   = this.platformConfiguration.onSocketClose;
+            mWebSocket.onmessage = this.platformConfiguration.onSocketMessage;
+            mWebSocket.onerror   = this.platformConfiguration.onSocketError;
+        }
+    };
+
+    return AdaptiloPlatform;    
+})();
